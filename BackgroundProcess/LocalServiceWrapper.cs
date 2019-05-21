8840873098cf62d759a4e22d42d02ceb2a1cc66d
Zheng_Xing <zheng_xing@dell.com>
2019-05-21 11:34:21 +0800

    Add project files.

---
+++ BackgroundProcess/LocalServiceWrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgroundProcess
+{
+    public class LocalServiceWrapper : ILocalService
+    {
+        public ServiceControllerStatus StartService(string name)
+        {
+            Console.WriteLine("Ready to start service……");
+            ServiceController controller = new ServiceController(name);
+
+            if (controller.Status == ServiceControllerStatus.Stopped)
+            {
+                Console.WriteLine("Starting service……");
+                controller.Start();
+                controller.WaitForStatus(ServiceControllerStatus.Running, TimeSpan.FromSeconds(5));
+                Console.WriteLine("Finished start service……");
+            }
+            return controller.Status;
+        }
+    }
+}
